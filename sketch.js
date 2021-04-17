eef4d3226fbc987e21bdb754514c4e31b6280199
Juri Perwuschin <juriperwuschin@gmail.com>
2021-04-17 08:03:58 +0200

    renaming title

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@
 var bubbles = [];
 
 function setup() {
-	createCanvas(1200, 800);
-    
+    createCanvas(1200, 800);
+
     for (let i = 0; i < 10000; i++) {
         bubbles.push(new Bubble(random(width), random(height), random(10, 40)));
     }
@@ -11,27 +11,27 @@ function setup() {
 
 function draw() {
     //frameRate(10);
-	background(0);
-    
-    for(let i = 0; i < bubbles.length; i++) {
+    background(0);
+
+    for (let i = 0; i < bubbles.length; i++) {
         bubbles[i].move();
         bubbles[i].show();
     }
 }
 
 class Bubble {
-    constructor (x, y, r) {
+    constructor(x, y, r) {
         this.x = x;
         this.y = y;
         this.r = r;
         this.v = 5;
     }
-    
+
     move() {
         this.x += random(-this.v, this.v);
         this.y += random(-this.v, this.v);
     }
-    
+
     show() {
         //stroke(255);
         //strokeWeight(4);
